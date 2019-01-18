6654da303de956a30d44ecd53c444d2577f697a9
dthinkcs <deliberatethink@gmail.com>
2019-01-19 01:27:54 +0530

    for each loop done

--- app.js
+++ app.js
@@ -7,7 +7,20 @@ const ejs = require("ejs");
 const homeStartingContent = "The Japanese word shokunin is defined by both Japanese and Japanese-English dictionaries as ‘craftsman’ or ‘artisan,’ but such a literal description does not fully express the deeper meaning. The Japanese apprentice is taught that shokunin means not only having technical skills, but also implies an attitude and social consciousness. … The shokunin has a social obligation to work his/her best for the general welfare of the people. This obligation is both spiritual and material, in that no matter what it is, the shokunin’s responsibility is to fulfill the requirement.The Japanese word shokunin is defined by both Japanese and Japanese-English dictionaries as ‘craftsman’ or ‘artisan,’ but such a literal description does not fully express the deeper meaning. The Japanese apprentice is taught that shokunin means not only having technical skills, but also implies an attitude and social consciousness. … The shokunin has a social obligation to work his/her best for the general welfare of the people. This obligation is both spiritual and material, in that no matter what it is, the shokunin’s responsibility is to fulfill the requirement";
 const aboutContent = "The Spirit Of The Shokunin: You owe it to the universe to SHOW UP and DO your art, CREATE your art.";
 const contactContent = "When you have focused and honestly worked and played the game you were meant to, you can listen to the diffuse whispers of the Shokunin Spirit."
-var posts = [];
+var posts =
+[
+  { title: 'Day 1',
+    content: 'Did some web dev basics, Javascript, Even Handlers, simple Games, JQuery Abstraction over JS' },
+  { title: 'Day 2',
+    content: 'Recursion Once Again Today' },
+  { title: 'Day 3',
+    content: 'Flight Today! So watched Awesome Stories. \nKind of saw Bahubaali Director. Decided to build JMessenger. Databases. Sample Space, Random Events, Mathusiasm' },
+  { title: 'Day 18',
+    content: "Built a Sudoku AI/Solver using Recursion/Backtracking/ExhaustiveSearch<br>Did some web design (spidey:) and web Development using MongoDB->mongooseODM, ExpressJS, NodeJS (and Front-End stuff as well)<br>Use Case Diagrams (Imtiaz), Came up with Alexa/Google-Home/Homepod Idea and absorbed some cool ads like Coke-v-Pepsi/Mac-v-PC/Win10-v-MacOSX, Discussed Graphic Design Freelance Work and Youtube Channel Strategy, Understood a little bit of TCP/IP Suite and Assembly Programming with ADC SBB Commands" }
+];
+
+
+
 const app = express();
 
 app.set('view engine', 'ejs');
